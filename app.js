6c9eab1fa6af346c8c3ab9d220fdb13801893aa7
Ned Zimmerman <ned@bight.dev>
2020-03-29 10:42:43 -0600

    No Vue

--- app.js
+++ app.js
@@ -1,54 +1,21 @@
 import { format, isPast, isAfter, isBefore, addHours } from "/web_modules/date-fns.js";
-import Vue from "/web_modules/vue/dist/vue.esm.browser.js";
 
-Vue.component('stream', {
-    template: '<article class="card" :data-status="item.status">\
-        <div class="card-image"><img :src="item.photo[0].url" alt="" loading="lazy" /></div>\
-        <time class="card-date" :datetime="item.startTime">{{ item.formattedStartTime }}</time>\
-        <h2><a rel="external" :href="item.link">{{ item.name }}</a></h2>\
-        <p>By <a rel="external" :href="item.creatorLink">{{ item.creatorName }}</a> on {{ item.platform }}</p>\
-        <div class="meta">\
-            <span class="subject">{{ item.subject }}</span>\
-            <span class="separator"> • </span>\
-            <span class="age-range">Ages {{ item.ageRange }}</span>\
-        </div>\
-        <div class="badge">\
-            <span class="label">{{ item.status }}</span>\
-            <svg viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">\
-                <g fill="currentColor">\
-                    <path d="M0,0 L60,0 C71.045695,0 80,8.954305 80,20 L80,80 L80,80 L0,0 Z"></path>\
-                </g>\
-            </svg>\
-        </div>\
-    </article>\
-    ',
-    props: ['item']
-  })
-
-var app = new Vue({
-    el: 'main',
-    data: {
-        items: []
-    },
-    mounted: function() {
-        fetch('/data.js')
-            .then(response => response.json())
-            .then(data => {
-                for (let index = 0; index < data.length; index++) {
-                    let stream = data[index];
-                    let formattedStartTime = `${format(new Date(stream.startTime), 'PP')} at ${format(new Date(stream.startTime), 'p')}`;
-                    stream.formattedStartTime = formattedStartTime;
-                    if (isPast(new Date(stream.endTime))) { // Stream end time is in the past.
-                        stream.status = 'done';
-                    }
-                    if (isAfter(new Date(stream.startTime), new Date()) && isBefore(new Date(stream.startTime), addHours(new Date(), 1))) { // Stream start time is within the next hour.
-                        stream.status = 'soon';
-                    }
-                    if (isAfter(new Date(), new Date(stream.startTime)) && isBefore(new Date(), new Date(stream.endTime))) { // Stream is live.
-                        stream.status = 'live';
-                    }
-                    this.items.push(stream);
-                }
-            })
-    }
-});
+const streams = document.querySelectorAll('article.card');
+if (streams.length > 0) {
+    streams.forEach(function(stream) {
+        const date = stream.querySelector('.card-date');
+        const startTime = date.getAttribute('datetime');
+        const endTime = date.dataset.end;
+        let formattedStartTime = `${format(new Date(startTime), 'PP')} at ${format(new Date(startTime), 'p')}`;
+        date.innerText = formattedStartTime;
+        if (isPast(new Date(endTime))) { // Stream end time is in the past.
+            stream.dataset.status = 'done';
+        }
+        if (isAfter(new Date(startTime), new Date()) && isBefore(new Date(startTime), addHours(new Date(), 1))) { // Stream start time is within the next hour.
+            stream.dataset.status = 'soon';
+        }
+        if (isAfter(new Date(), new Date(startTime)) && isBefore(new Date(), new Date(endTime))) { // Stream is live.
+            stream.dataset.status = 'live';
+        }
+    });
+}
