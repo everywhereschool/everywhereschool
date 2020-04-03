be945017d43c62b8bb762a448f6d644c509ef25d
Ned Zimmerman <ned@bight.dev>
2020-04-02 22:43:54 -0600

    Split days [WIP]

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-import { format, isPast, isAfter, isBefore, addHours } from "/web_modules/date-fns.js";
+import { format, isPast, isAfter, isBefore, isToday, isTomorrow, addHours } from "/web_modules/date-fns.js";
 
 const streams = document.querySelectorAll('article.card');
 if (streams.length > 0) {
+    let current = false;
     streams.forEach(function(stream) {
         const date = stream.querySelector('.card-date');
         const startTime = date.getAttribute('datetime');
         const endTime = date.dataset.end;
         let formattedStartTime = `${format(new Date(startTime), 'PP')} at ${format(new Date(startTime), 'p')}`;
         date.innerText = formattedStartTime;
+        // Stream is today.
+        if (isToday(new Date(startTime))) {
+            if (!current) {
+                const heading = document.createElement('h2');
+                heading.className = 'clearfix';
+                heading.textContent = 'Today’s Livestreams';
+                stream.parentNode.insertBefore(heading, stream);
+            }
+            current = 'today';
+        }
+        // Stream is tomorrow.
+        if (isTomorrow(new Date(startTime))) {
+            if (current !== 'tomorrow') {
+                const heading = document.createElement('h2');
+                heading.className = 'clearfix';
+                heading.textContent = 'Tomorrow’s Livestreams';
+                stream.parentNode.insertBefore(heading, stream);
+            }
+            current = 'tomorrow';
+        }
         // Stream end time is in the past.
         if (isPast(new Date(endTime))) {
             stream.dataset.status = 'done';
