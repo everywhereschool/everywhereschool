83418eb2dd6b9250e50b9d7f41daf8bca9e11f1c
Ned Zimmerman <ned@bight.dev>
2020-08-06 20:43:51 -0300

    feat: add filters, remove times and dates (#29)
    * feat: add filters (WIP)
    
    * tweaks to card sizes on mobile
    
    * fix: mobile css for filters
    
    * feat: remove schedule
    
    Co-authored-by: Nealevf <neale@rogueamoeba.com>

--- _data/streams.js
+++ _data/streams.js
@@ -5,15 +5,12 @@ const airtable = require('airtable');
 const Bottleneck = require('bottleneck');
 const base = new airtable({apiKey}).base('appIDPnCgGApCGUtF');
 
-const compareStreams = (a, b) => {
-	const streamA = new Date(a.startTime);
-	const streamB = new Date(b.startTime);
-	
+const compareStreams = (a, b) => {	
 	let comparison = 0;
 
-	if (streamA.getTime() > streamB.getTime()) {
+	if (a.name > b.name) {
 		comparison = 1;
-	} else if (streamA.getTime() < streamB.getTime()) {
+	} else if (a.name < b.name) {
 		comparison = -1;
 	}
 
@@ -25,21 +22,17 @@ module.exports = async function() {
 		const limiter = new Bottleneck({minTime: 1000 / 5});
 		const streams = [];
 
-		limiter.wrap(base('Streams').select({
-			filterByFormula: "IS_AFTER({End Time}, NOW())"
-		}).eachPage(function page(records, fetchNextPage) {
+		limiter.wrap(base('Streams').select().eachPage(function page(records, fetchNextPage) {
 			records.forEach(function(record) {
 				const name = record.get('Name');
 				const link = record.get('Link');
 				const photo = record.get('Photo');
-				const startTime = record.get('Start Time');
-				const endTime = record.get('End Time');
 				const creatorName = record.get('Creator Name');
 				const creatorLink = record.get('Creator Link');
 				const platform = record.get('Platform') || false;
 				const subject = record.get('Subject');
 				const ageRange = record.get('Age Range');
-				streams.push({name, link, photo, startTime, endTime, creatorName, creatorLink, platform, subject, ageRange});
+				streams.push({name, link, photo, creatorName, creatorLink, platform, subject, ageRange});
 			});
 			fetchNextPage();
 		}, function done(err) {
@@ -47,7 +40,20 @@ module.exports = async function() {
 				console.error(err);
 				reject(error);
 			}
-			resolve({items: streams.sort(compareStreams)});
+
+			const subjects = streams.reduce((accumulator, currentStream) => {				
+				if (!accumulator.includes(currentStream.subject)) {
+					accumulator.push(currentStream.subject);
+				}
+		
+				return accumulator;
+			}, []);
+
+			resolve({
+				items: streams.sort(compareStreams),
+				ageRanges: ["3+", "6+", "9+", "12+", "Teens+", "All Ages"],
+				subjects: subjects.sort()
+			});
 		}));
 	});
 }
--- app.js
+++ app.js
@@ -1,129 +1,32 @@
-import { format, isPast, isAfter, isBefore, isToday, isTomorrow, addHours, addDays, endOfTomorrow } from "/web_modules/date-fns.js";
-
-function insertHeading(stream, label) {
-    const heading = document.createElement('h2');
-    heading.className = 'clearfix date-title';
-    heading.textContent = label;
-    stream.parentNode.insertBefore(heading, stream);
-}
-
-function formatDates(streams) {
-    streams.forEach(function(stream) {
-        const date = stream.querySelector('.card-date');
-        const startTime = date.getAttribute('datetime');
-        date.innerText = `${format(new Date(startTime), 'PP')} at ${format(new Date(startTime), 'p')}`;
-    });
-}
-
-function groupStreams(streams) {
-    let offset = false;
-    streams.forEach(function(stream) {
-        const startTime = stream.querySelector('.card-date').getAttribute('datetime');
-        // Stream is today.
-        if (isToday(new Date(startTime))) {
-            if (!offset) {
-                insertHeading(stream, 'Today’s Livestreams');
-                offset = '0';
-            }
-        }
-
-        // Stream is tomorrow.
-        if (isTomorrow(new Date(startTime))) {
-            if (!offset || offset === '0') {
-                insertHeading(stream, 'Tomorrow’s Livestreams');
-                offset = '1';
-            }
-        }
-
-        // Stream is in two days.
-        if (isAfter(new Date(startTime), endOfTomorrow()) && isBefore(new Date(startTime), addDays(endOfTomorrow(), 1))) {
-            if (offset === '1') {
-                insertHeading(stream, `${format(new Date(startTime), 'EEEE')}’s Livestreams`);
-                offset = '2';
-            }
-        }
-        // Stream is in three days.
-        if (isAfter(new Date(startTime), addDays(endOfTomorrow(), 1)) && isBefore(new Date(startTime), addDays(endOfTomorrow(), 2))) {
-            if (offset === '2') {
-                insertHeading(stream, `${format(new Date(startTime), 'EEEE')}’s Livestreams`);
-                offset = '3';
-            }
-        }
-        // Stream is in four days.
-        if (isAfter(new Date(startTime), addDays(endOfTomorrow(), 2)) && isBefore(new Date(startTime), addDays(endOfTomorrow(), 3))) {
-            if (offset === '3') {
-                insertHeading(stream, `${format(new Date(startTime), 'EEEE')}’s Livestreams`);
-                offset = '4';
-            }
-        }
-        // Stream is in five days.
-        if (isAfter(new Date(startTime), addDays(endOfTomorrow(), 3)) && isBefore(new Date(startTime), addDays(endOfTomorrow(), 4))) {
-            if (offset === '4') {
-               insertHeading(stream, `${format(new Date(startTime), 'EEEE')}’s Livestreams`);
-               offset = '5';
-            }
-        }
-        // Stream is in six days.
-        if (isAfter(new Date(startTime), addDays(endOfTomorrow(), 4)) && isBefore(new Date(startTime), addDays(endOfTomorrow(), 5))) {
-            if (offset === '5') {
-                insertHeading(stream, `${format(new Date(startTime), 'EEEE')}’s Livestreams`);
-                offset = '6';
-            }
-        }
-        // Stream is next week.
-        if (isAfter(new Date(startTime), addDays(endOfTomorrow(), 5)) && isBefore(new Date(startTime), addDays(endOfTomorrow(), 13))) {
-            if (offset === '6') {
-                insertHeading(stream, 'Next Week’s Livestreams');
-                offset = 'nextweek';
-            }
-        }
-        // Stream is later.
-        if (isAfter(new Date(startTime), addDays(endOfTomorrow(), 13))) {
-            if (offset === 'nextweek') {
-                insertHeading(stream, 'Future Livestreams');
-                offset = 'later';
-            }
-        }
-    });
-}
-
-function updateStreams() {
+function filterStreams() {
     const streams = document.querySelectorAll('article.card');
+    const ageRange = document.getElementById('ages').value !== '' ? document.getElementById('ages').value : false;
+    const subject = document.getElementById('subject').value !== '' ? document.getElementById('subject').value : false;
 
-    streams.forEach(function(stream) {
-        const date = stream.querySelector('.card-date');
-        const startTime = date.getAttribute('datetime');
-        const endTime = date.dataset.end;
-        
-        // Stream end time is in the past.
-        if (isPast(new Date(endTime))) {
-            stream.dataset.status = 'done';
-            stream.querySelector('.badge .label').innerText = 'done';
+    const matchedStreams = [...streams].filter(stream => {
+        if (ageRange && subject) {
+            return stream.dataset.ages === ageRange && stream.dataset.subject === subject;
+        } else if (ageRange) {
+            return stream.dataset.ages === ageRange;
+        } else if (subject) {
+            return stream.dataset.subject === subject;
+        } else {
+            return stream;
         }
-        // Stream start time is within the next hour.
-        if (isAfter(new Date(startTime), new Date()) && isBefore(new Date(startTime), addHours(new Date(), 1))) {
-            stream.dataset.status = 'soon';
-            stream.querySelector('.badge .label').innerText = 'soon';
-        }
-        // Stream is live.
-        if (isAfter(new Date(), new Date(startTime)) && isBefore(new Date(), new Date(endTime))) {
-            stream.dataset.status = 'live';
-            stream.querySelector('.badge .label').innerText = 'live';
+    });
+
+    streams.forEach((stream) => {
+        if (!matchedStreams.includes(stream)) {
+            stream.setAttribute('hidden', '');
+        } else {
+            stream.removeAttribute('hidden');
         }
     });
 }
 
-function timedUpdate() {
-    updateStreams();
-    setTimeout(timedUpdate, 60000);
-}
+document.addEventListener('input', function (event) {
+    if (event.target.id !== 'ages' && event.target.id !=='subject') return;
 
-const streams = document.querySelectorAll('article.card');
-
-if (streams.length > 0) {
-    formatDates(streams);
-    groupStreams(streams);
-    timedUpdate();
-}
-
+	filterStreams();
+}, false);
 
